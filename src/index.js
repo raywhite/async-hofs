ae1a1cb03914b9581695615299f74b41f3789c1d
axdg <axdg@dfant.asia>
2017-11-27 13:09:39 +1100

    notes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+/**
+ * This one is in no way an async util... but I'm using it heaps
+ * and just want somewhere safe to export form ATM.
+ *
+ * NOTE: This is a fast simple implementation of a function
+ * that can memoizes against a single (`string || number`)
+ * param.
+ *
+ * @param {Function}
+ * @returns {Function}
+ */
 module.exports.memoize = function (fn) {
   const cache = {}
 
+  /**
+   * @param {String|Number}
+   * @returns {Mixed}
+   */
   const m = function (p) {
     if (cache[p]) return cache[p]
     cache[p] = fn(p)
@@ -14,6 +29,18 @@ module.exports.memoize = function (fn) {
   return m
 }
 
+/**
+ * Creates a pool of promises (AKA, the results of async
+ * fn invokations) to distribute work and help with syncronising
+ * concurrency.
+ *
+ * NOTE: If the provided coroutine requires params, use
+ * `Array.prototype.bind` to set them.
+ *
+ * @param {Function}
+ * @param {Number}
+ * @returns {Promise => Array}
+ */
 module.exports.createAsyncFnPool = function (fn, concurrency = 1) {
   const pool = []
 
@@ -29,7 +56,23 @@ module.exports.createAsyncFnPool = function (fn, concurrency = 1) {
   return Promise.all(pool)
 }
 
+/**
+ * Wraps a function for retrying....  takes the retry limit.
+ *
+ * TODO: This only supports retrying `limit` times, all retiries
+ * happen immediately once the previous attempt failed - but if
+ * this function also accepted a curve, and a limt, then we could make
+ * it do retries at linear or exponential intervals.
+ *
+ * @param {Function}
+ * @param {Number}
+ * @returns {Function}
+ */
 module.exports.createRetrierFn = function (fn, limit = 2) {
+  /**
+   * @param {...Mixed}
+   * @returns {Mixed}
+   */
   return function (arg) {
     return new Promise(function (resolve, reject) {
       const recurse = function (err, r) {
@@ -46,12 +89,34 @@ module.exports.createRetrierFn = function (fn, limit = 2) {
   }
 }
 
+/**
+ * @param {Mixed}
+ * @returns {Boolean}
+ */
 const isPromise = function (x) {
   return x instanceof Promise
 }
 
+/**
+ * @pararm {Function}
+ * @returns {Function}
+ */
 const createSequencer = function (method) {
+  /**
+   * Sequence or compose the provided args, even if they
+   * aren't functions that return promises.
+   *
+   * NOTE: The only difference between sequence and compose
+   * is the direction in which the args are consumed.
+   *
+   * @param {...Function}
+   * @returns {Function}
+   */
   return function (...fns) {
+    /**
+     * @param {Mixed}
+     * @returns {Promise => Mixed}
+     */
     return function (v) {
       return new Promise(function (resolve, reject) {
         const recurse = function (_v) {
@@ -76,22 +141,35 @@ const createSequencer = function (method) {
   }
 }
 
+// `sequence` => left to right <= `compose`.
 module.exports.sequence = createSequencer(Array.prototype.shift)
 module.exports.compose = createSequencer(Array.prototype.pop)
-
-const createError = function (str) {
-  const err = new Error(str)
-  err.type = str
-  return err
-}
 
 // TODO: This will consume the stream... so has to error.
 module.exports.buffer = (function () {
   const LIMIT_EXCEEDED = 'byte limit exceeded'
   const SECOND_STREAM_CONSUMER = 'stream already consumed'
 
+  /**
+   * @param {String}
+   * @returns {Error}
+   */
+  const createError = function (str) {
+    const err = new Error(str)
+    err.type = str
+    return err
+  }
+
   const map = new WeakMap()
 
+  /**
+   * Buffers a readable stream - default to erroring when more than
+   * 1MB is consumed.
+   *
+   * @param {stream.Readable}
+   * @param {Number}
+   * @returns {Promise => Buffer}
+   */
   const buffer = function (readable, limit = (1000 * 1024)) {
     let len = 0
     const chunks = []
@@ -117,6 +195,7 @@ module.exports.buffer = (function () {
     })
   }
 
+  // NOTE: exported as constants to allow assertion.
   buffer.LIMIT_EXCEEDED = LIMIT_EXCEEDED
   buffer.SECOND_STREAM_CONSUMER = SECOND_STREAM_CONSUMER
 
