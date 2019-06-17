34e2ae47c6f0fe0094a59702fe97b7cb48f23827
axdg <axdg@dfant.asia>
2019-06-17 18:11:59 +1000

    simpler implementation

--- src/retrier.js
+++ src/retrier.js
@@ -1,5 +1,3 @@
-const { isNumber } = require('./utilities')
-
 const createLinear = function (constants = {}) {
   const { m = 1, b = 0 } = constants
 
@@ -27,49 +25,43 @@ const createExponential = function (constants = {}, m = 1) {
 module.exports.createExponential = createExponential
 
 /**
- * @param {Function}
- * @param {Function|Number}
- * @param {Number}
- * @param {Function}
+ * Wraps an `async` function for async retries as per the logic given in `curve`.
+ * Curve is expected to be a callback which will be called at each failed
+ * iteration with `err` (the error thrown by the wrapped function) and `count`
+ * which is the count of total attempts to `resolve` - it should either throw
+ * (should no more retries be required) or return an integer dictating the
+ * number of milliseconds to sleep before re-attempting resolution.
+ *
+ * @param {Function} fn
+ * @param {Function|Object} curve
  * @returns {Function}
  */
-const createRetrierFn = function (fn, curve = 2, limit = 2, shouldRetry = undefined) {
-  if (isNumber(curve)) {
-    limit = curve
-    curve = zero
+const createRetrierFn = function (fn, curve) {
+  if (!curve) {
+    curve = function (err, count) {
+      if (count > 1) throw err
+      return 0
+    }
   }
 
-  return function () {
-    const args = Array.prototype.slice.call(arguments)
+  return function (...args) {
+    let attempt = 0
 
-    return new Promise(function (resolve, reject) {
-      (function recurse(attempt) {
-        function retry(error) {
-          const errorCount = attempt + 1
-
-          if (limit && errorCount >= limit) return reject(error)
-
-          if (shouldRetry && !shouldRetry(error)) return reject(error)
-
-          return recurse(errorCount)
+    const recurse = function (resolve, reject) {
+      return fn(...args).then(resolve).catch(function (err) {
+        let timeout = curve(err, ++attempt)
+        if (timeout < 0) return reject(err)
+        try {
+          if (err <= 0) return recurse(resolve, reject)
+          return setTimeout(recurse, timeout, resolve, reject)
+        } catch (_err) {
+          return reject(_err)
         }
+      })
+    }
 
-        setTimeout(function () {
-          try {
-            Promise
-              .resolve(fn.apply(null, args))
-              .then(resolve)
-              .catch(function (asyncErr) {
-                return retry(asyncErr)
-              })
-          } catch (syncErr) {
-            retry(syncErr)
-          }
-        }, curve(attempt))
-      }(null, 0))
-    })
+    return new Promise(recurse)
   }
 }
 
-module.exports.retry = createRetrierFn
 module.exports.createRetrierFn = createRetrierFn
